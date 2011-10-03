f441dcf60006f5c259ffaea2fa08b85e45a1184c
Franziska Hinkelmann <fhinkel@vt.edu>
2011-10-03 17:08:40 -0400

    notepad for notes and some rounded corners

--- SimLab.js
+++ SimLab.js
@@ -2,4 +2,6 @@ $(document).ready(function() {
 	$('.tt').addClass('tooltip');
 	$('.tt').before('<img src="./qm.jpg" alt="tooltip" name="tooltip" height="20" class="trigger">');
 	$(".trigger").tooltip();
+	
+//	$("[name='wiring']").css('padding', "15px").css('border',"2px solid");
 } );
